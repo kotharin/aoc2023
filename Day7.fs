17176a058eaedb5a69aba7dd89d1a3245b282887
Neerav Kothari <neerav.kothari@gmail.com>
2023-12-26 16:24:08 -0500

    Day7 Part1 - NOT FINISHED. Need to write ranking/sorting function

---
+++ Day7.fs
@@ -0,0 +1,92 @@
+namespace Day7
+
+module Shared =
+
+    type Rank =
+        | HighCard = 1
+        | OnePair = 2
+        | TwoPair = 3
+        | ThreeOfAKind = 4
+        | FullHouse = 5
+        | FourOfAKind = 6
+        | FiveOfAKind = 7
+
+    let is2PairOr3ofAKind (cards:string array) =
+        let rec is2or3 (remainingCards:string array) =
+            // take first card and remove all instances in the hand
+            let first = remainingCards.[0]
+            let newRemainingCards =
+                remainingCards
+                |> Array.filter (fun c -> c <> first)
+            if (remainingCards.Length = 5) then
+                if (newRemainingCards.Length = 2) then
+                    Rank.ThreeOfAKind
+                elif (newRemainingCards.Length = 3) then
+                    Rank.TwoPair
+                else
+                    is2or3 newRemainingCards
+            else
+                if (newRemainingCards.Length = 2) then
+                    Rank.TwoPair
+                else
+                    Rank.ThreeOfAKind
+        is2or3 cards
+
+    let getRank (cards:string array) =
+        let set = Set.ofArray cards
+
+        match Set.count set with
+        | 1 -> Rank.FiveOfAKind
+        | 2 -> 
+            // four of a kind or full house
+            // Remove any one of the cards
+            // If what remains is a of length 1 or 4 
+            // its 4 of a kind
+            let replace = Set.minElement set
+            let replacedCards =
+                cards
+                |> Array.filter(fun c -> c = replace)
+            if ((replacedCards.Length = 4) || (replacedCards.Length = 1)) then
+                Rank.FourOfAKind
+            else
+                Rank.FullHouse
+        | 3 -> 
+            // Three of a kind or 2 pair
+            is2PairOr3ofAKind cards
+        | 4 ->  Rank.OnePair
+        | _ -> Rank.HighCard
+
+    type Hand = {
+        Cards: string array
+        Bid: int
+        TypeRank: Rank
+    } with
+        static member parse (line:string) =
+            let chunks = line.Split([|' '|])
+
+            let cards =
+                chunks.[0].Trim().ToCharArray()
+                |> Array.map(string)
+            
+            let bid = int(chunks.[1].Trim())
+
+            let rank = getRank cards
+
+            {Cards = cards; Bid = bid; TypeRank = rank}
+
+module Part1 =
+    open System.IO
+    open Shared
+
+    //let rankHands (han)
+    let solution inputFile =
+        
+        let lines = File.ReadAllLines inputFile
+
+        let hands =
+            lines
+            |> Array.map(Hand.parse)
+            |> Array.groupBy (fun hand -> hand.TypeRank)
+            |> Array.sortBy fst
+        printfn "hands:%A" hands
+        0
--- Program.fs
+++ Program.fs
@@ -1,10 +1,10 @@
 ﻿module Program =
 
-    open Day6
+    open Day7
     
     let [<EntryPoint>] main _ =
         
-        let answer = Day6.Part2.solution "Day6-1.txt"
+        let answer = Day7.Part1.solution "Day7-1.txt"
         printfn "%i" answer
         //let a = Day3.Part1.getAdjacentPositions 9 9 {X = 4; Y = 5}
         //printfn "a:%A" a
