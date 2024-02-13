ab931ed493486311cdff3475cedeb05921dbb0e9
Neerav Kothari <neerav.kothari@gmail.com>
2024-02-13 16:39:29 -0500

    Day8 Part2. Calculated LCM manually

--- Day8.fs
+++ Day8.fs
@@ -1,13 +1,12 @@
 namespace Day8
 
-module Part1 =
-
-    open System.IO
+module Shared =
 
     type Node = {
         Label: string
         Left: string
         Right: string
+        EndsWithZ: bool
     } with 
         static member parse (line:string) =
             // split on the =
@@ -16,8 +15,11 @@ module Part1 =
             // parse thes second part
             let moreParts = parts[1].Replace("(", "").Replace(")", "").Split([|','|])
             let left, right = moreParts[0].Trim(), moreParts[1].Trim()
-
-            {Node.Label = label; Left = left; Right = right}
+            let ewz =
+                if (label.Substring(2,1) = "Z") then
+                    true
+                else false
+            {Node.Label = label; Left = left; Right = right; EndsWithZ = ewz}
 
     // traverse to the next node based on teh instruction
     let traverseInstruction instruction nodesMap node =
@@ -28,6 +30,10 @@ module Part1 =
             else node.Left
         Map.find nextNodeLabel nodesMap
 
+module Part1 =
+
+    open System.IO
+    open Shared
     let countStepTillEnd (instructions:char array) nodesMap start finish =
 
         let rec countTillFinish insPos currentNode count =
@@ -64,3 +70,81 @@ module Part1 =
         let steps = countStepTillEnd instructions nodesMap startingNode finishingNode
 
         steps
+
+module Part2 =
+
+    open System.IO
+    open Shared
+    open FSharp.Collections
+    open System
+
+    let allNodesEndWith nodes =
+        let compliantNodes =
+            nodes
+            |> Array.Parallel.filter (fun n -> n.EndsWithZ)
+
+        let cl = compliantNodes.Length
+        if ((cl > 0) && (compliantNodes.Length % 4 = 0)) then
+            printfn "----- ewz: %i" compliantNodes.Length
+
+        compliantNodes.Length = nodes.Length
+
+    let countSteps (instructions:char array) nodesMap startingNodes =
+        
+        let rec countAllSteps insPos currentNodes count =
+            if ((count> 0) && (allNodesEndWith currentNodes)) then
+                printfn "end node: %A" currentNodes
+                count
+            else
+                //if (count%1000000 = 0) then
+                    //printfn "step count:%i" count
+                // move nodes one more instruction
+                let newInsPos =
+                    if (insPos >= instructions.Length) then
+                        0
+                    else insPos
+                let newInstruction = instructions[newInsPos]
+
+                let newCurrentNodes =
+                    currentNodes
+                    |> Array.Parallel.map(fun node ->
+                        traverseInstruction newInstruction nodesMap node
+                    )
+                //printfn "new nodes:%A" newCurrentNodes
+                countAllSteps (newInsPos + 1) newCurrentNodes (count + 1)
+        
+        countAllSteps 0 startingNodes 0
+
+    let solution inputFile =
+        let lines = File.ReadAllLines(inputFile)
+
+        let instructions = lines[0].ToCharArray()
+
+        let nodesList, allANodes = 
+            Array.skip 2 lines
+            |> Array.fold(fun (nodes, allANodes) l ->
+                let node = Node.parse l
+                let newNodes =  (node.Label, node)::nodes
+                let newAllANodes =
+                    if (node.Label.Substring(2,1) = "A") then
+                        node::allANodes
+                    else allANodes
+
+                newNodes, newAllANodes
+            ) (List.empty, List.empty)
+
+        let nodesMap = Map.ofList nodesList
+        let allANodesArray = allANodes |> Array.ofList
+        printfn "starting nodes: %i" (allANodesArray.Length)
+
+        
+        let commonCycle =
+            allANodesArray
+            |> Array.map(fun node ->
+                countSteps instructions nodesMap [|node|]
+            )
+
+        // find the LCM of the below numbers
+        printfn "cc:%A" commonCycle
+
+        0
--- Program.fs
+++ Program.fs
@@ -4,7 +4,7 @@
     
     let [<EntryPoint>] main _ =
         
-        let answer = Day8.Part1.solution "Day8-1.txt"
+        let answer = Day8.Part2.solution "Day8-1.txt"
         printfn "%i" answer
         //let a = Day3.Part1.getAdjacentPositions 9 9 {X = 4; Y = 5}
         //printfn "a:%A" a
